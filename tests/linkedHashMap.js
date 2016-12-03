bc7b89872a87c63ece578865d371e65b511a2b8b
Alexandre Morin <aggmorin@gmail.com>
2016-12-03 18:35:58 +0100

    Change test name

--- tests/linkedHashMap.js
+++ tests/linkedHashMap.js
@@ -6,7 +6,7 @@
 const assert = require('assert');
 const LinkedHashMap = require('../lib/LinkedHashMap.js');
 
-describe('LinkedHashMap', function() {
+describe('Testing LinkedHashMap', function() {
 
   function checkOrder(map, expected) {
     found = "";
